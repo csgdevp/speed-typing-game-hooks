e2838f461d36fdfc41103ff9577414a90d67caf7
Chandra Gonuguntla <csgdevp@gmail.com>
2021-04-07 15:49:59 -0400

    Adding useEffect
    Changing states based on use interactions. Added  useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
+  const STARTING_TIME = 7;
   const [text, setText] = useState("");
-  const [timeRemaining, setTimeRemaining] = useState(5);
+  const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME);
+  const [isTimeRunning, setIsTimeRunning] = useState(false);
+  const [wordCount, setWordCount] = useState(0);
+
   function handleChange(e) {
     const { value } = e.target;
     setText(value);
@@ -14,24 +18,41 @@ function App() {
     return filteredWords.length;
   }
 
+  function startGame() {
+    setIsTimeRunning(true);
+    setTimeRemaining(STARTING_TIME);
+    setText("");
+  }
+
+  function endGame() {
+    setIsTimeRunning(false);
+    const numWords = calculateWords(text);
+    setWordCount(numWords);
+  }
+
   useEffect(() => {
-    if (timeRemaining > 0) {
-      const timeRemain = setTimeout(() => {
+    if (isTimeRunning && timeRemaining > 0) {
+      setTimeout(() => {
         setTimeRemaining((prevTime) => prevTime - 1);
       }, 1000);
-      return () => {
-        clearTimeout(timeRemain);
-      };
+    } else if (timeRemaining === 0) {
+      endGame();
     }
-  }, [timeRemaining]);
+  }, [timeRemaining, isTimeRunning]);
 
   return (
     <div>
       <h1>Speed Typing Game</h1>
-      <textarea value={text} onChange={handleChange} />
+      <textarea
+        disabled={!isTimeRunning}
+        value={text}
+        onChange={handleChange}
+      />
       <h4>Time Remaining: {timeRemaining}</h4>
-      <button>Start</button>
-      <h1>Your Word Count: {calculateWords(text)}</h1>
+      <button onClick={startGame} disabled={isTimeRunning}>
+        Start
+      </button>
+      <h1>Your Word Count: {wordCount}</h1>
     </div>
   );
 }
